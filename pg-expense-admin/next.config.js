3e99da6ec69b83430044bd48f0ad13a42b2160f6
Vivek <vivekpramanikbundu@gmail.com>
2026-04-10 00:54:10 +0530

    feat: added vercel file

--- pg-expense-admin/next.config.js
+++ pg-expense-admin/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const backendOrigin = process.env.BACKEND_ORIGIN
+const backendOrigin = process.env.BACKEND_ORIGIN;
 
 const nextConfig = {
   async rewrites() {
+    if (!backendOrigin) return [];
     return [
       {
         source: '/api/v1/:path*',
@@ -11,4 +12,5 @@ const nextConfig = {
     ];
   },
 };
+
 module.exports = nextConfig;
